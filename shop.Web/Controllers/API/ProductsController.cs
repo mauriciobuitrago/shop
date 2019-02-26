3ca53d6e31e40971795c3a08896605492fdad140
rokect <rokect@DESKTOP-CMD15OT>
2019-02-26 13:33:45 -0500

    API products controllers

---
+++ shop.Web/Controllers/API/ProductsController.cs
@@ -0,0 +1,29 @@
+﻿
+namespace shop.Web.Controllers.API
+{
+    using Microsoft.AspNetCore.Mvc;
+    using shop.Web.Data;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    [Route("api/[Controller]")]
+
+    public class ProductsController : Controller
+
+    {
+        private readonly IProductRepository productRepository;
+
+        public ProductsController(IProductRepository productRepository)
+        {
+            this.productRepository = productRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            return Ok(this.productRepository.GetAll());
+        }
+    }
+}
