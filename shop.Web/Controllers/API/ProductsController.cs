4804aa6395c8de5699df0fb3f221e178a0acecd1
rokect <rokect@DESKTOP-CMD15OT>
2019-03-01 12:14:28 -0500

    API Products Controllers

--- shop.Web/Controllers/API/ProductsController.cs
+++ shop.Web/Controllers/API/ProductsController.cs
@@ -3,10 +3,6 @@ namespace shop.Web.Controllers.API
 {
     using Microsoft.AspNetCore.Mvc;
     using shop.Web.Data;
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Threading.Tasks;
 
     [Route("api/[Controller]")]
 
