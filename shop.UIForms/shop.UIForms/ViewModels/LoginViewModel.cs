9595c77babce80f39bce24de62cf23a150363d0a
rokect <rokect@DESKTOP-CMD15OT>
2019-03-05 13:48:49 -0500

    Implement of xamarin form and login

---
+++ shop.UIForms/shop.UIForms/ViewModels/LoginViewModel.cs
@@ -0,0 +1,17 @@
+﻿
+
+using System.Windows.Input;
+
+namespace Shop.UIForms.ViewModels
+{
+    public class LoginViewModel
+    {
+        public LoginViewModel Login { get; set; }
+
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public ICommand LoginCommand { get; }
+
+    }
+}
